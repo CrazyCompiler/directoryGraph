27b0712c7dafdf1adb27cc3918ec9451072c3280
Viraj Parab <parabveer@gmail.com>
2016-08-21 18:47:17 +0530

    Viraj - Add directoryGraph component

---
+++ assets/javascript/components/DirectoryStructure.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class DirectoryStructure extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        This is Graph
+      </div>
+    );
+  }
+}
--- assets/javascript/components/Wrapper.js
+++ assets/javascript/components/Wrapper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
+import DirectoryStructure from "./DirectoryStructure";
 
 export default class Wrapper extends React.Component {
 
@@ -8,6 +9,7 @@ export default class Wrapper extends React.Component {
     return (
       <div>
           <Header />
+          <DirectoryStructure />
           <Footer />
       </div>
     );
