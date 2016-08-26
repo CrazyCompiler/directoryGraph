277d7edcbd0435642d905ac0199f9b124843d142
viraj parab <virajpa@thoughtworks.com>
2016-08-26 10:24:33 +0530

    Viraj - store should provide directory structure

--- javascript/store/AppStore.js
+++ javascript/store/AppStore.js
@@ -1,7 +1,25 @@
 import Reflux from "reflux";
 import AppActions from "../actions/AppActions";
+import os from 'os';
+import shell from 'shelljs/global';
+
 var AppStore = Reflux.createStore({
     listenables: [AppActions],
+
+    getDirectoryData: function(fromPath) {
+        if(fromPath){
+            return ls('-l',fromPath);
+        }
+        return ls('-l',os.homedir());
+    },
+
+    onGetDirectoryDetails : function(fromPath) {
+    let directoryData = this.getDirectoryData(fromPath);
+    this.trigger({
+        action : "directoryDetails",
+        data : directoryData
+    });
+},
 });
 
 module.exports = AppStore;
--- javascript/store/__tests__/AppStore-Test.js
+++ javascript/store/__tests__/AppStore-Test.js
@@ -2,10 +2,12 @@
 
 //TODO: - app-store should get the details about the users home directory.
 //TODO: - app-store should get the directory details from any directory.
-
 import React from 'react';
 import AppStore from '../AppStore';
 import AppActions from '../../actions/AppActions';
+import _ from 'underscore'
+import os from 'os';
+import sinon from 'sinon';
 
 describe('AppStore', function() {
 
@@ -13,9 +15,29 @@ describe('AppStore', function() {
     expect(AppStore.listenables).toContain(AppActions);
   });
 
-  it('should provide the details about the users home directory', function() {
+  it('should provide the details about the users home directory, if no directory provided', function() {
+      var spy = sinon.spy(os, "homedir");
+      let subscription = AppStore.listen((payload) => {
+            expect(payload.action).toBe("directoryDetails");
+            expect(payload.data).not.toBeNull;
+            expect(spy.called).toBeTruthy();
+      });
 
-  });
+      AppStore.onGetDirectoryDetails();
+    });
+    it('should provide the details about the directory provided', function() {
+      mkdir('demoTestDirectory');
+      let subscription = AppStore.listen((payload) => {
+            let demoDirectory = _.find(payload.data,(element) => {
+                return element.name == 'demoTestDirectory';
+            });
+            expect(payload.action).toBe("directoryDetails");
+            expect(payload.data).not.toBeNull;
+            expect(demoDirectory.name).toBe('demoTestDirectory');
+            rm('-rf','demoTestDirectory');
+      });
 
+      AppStore.onGetDirectoryDetails('./');
+    })
 });
 
